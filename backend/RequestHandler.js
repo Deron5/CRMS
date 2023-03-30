cf960625711e0c2d650701614dc458564319a1ce
Deron Worrell <justtimb55@gmail.com>
2023-03-29 20:37:21 -0500

    backend-stuff

--- backend/RequestHandler.js
+++ backend/RequestHandler.js
@@ -17,6 +17,33 @@ conn.connect(function (err) {
     console.log("Connected!");
 });
 
+/** Queries //just didn't bother making them variables yet
+ customer rentals;
+select * from rentals, customers, vehicle_types, vehicles where rentals.customer_id = customers.customer_id and rentals.vehicle_id = vehicles.license_plate_number and vehicles.vehicle_type = vehicle_types.vin; 
+
+rental history report;
+select * from rentals, vehicle_types, vehicles where rentals.vehicle_id = vehicles.license_plate_number and vehicles.vehicle_type = vehicle_types.vin; 
+
+late return report;
+select * from rentals, customers where rentals.customer_id = customers.customer_id and rentals.return_condition is null 
+
+
+
+vehicle turnover report
+select * from rentals,vehicles, vehicle_types where rentals.vehicle_id = vehicles.license_plate_number and vehicles.vehicle_type = vehicle_types.vin;
+
+vehicle availability report (how would i do this?)
+
+
+default rental query
+select * from rentals,vehicles, vehicle_types where rentals.vehicle_id = ? and vehicles.vehicle_type = vehicle_types.vin;
+
+
+default customer_rental query
+ */
+
+
+
 const NONE_ = "No Data!";
 
 class RequestHandler{
