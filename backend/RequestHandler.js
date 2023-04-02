5a6773fd93ae616f3d4b9fa15f89e0ac675371da
Deron Worrell <justtimb55@gmail.com>
2023-04-02 18:49:18 -0500

    finished rental turnover backend;

--- backend/RequestHandler.js
+++ backend/RequestHandler.js
@@ -18,6 +18,20 @@ conn.connect(function (err) {
     console.log("Connected!");
 });
 
+const NO_RESULTS = 'empty result-set';
+
+const RENTALS_QUERY = "select * from rentals where vehicle_id = ?";
+
+const FIRST_RENTAL = "select * from rentals order by vehicle_id limit 1";
+const NEXT_RENTAL_QUERY = "select * from rentals where vehicle_id = (select min(vehicle_id) from rentals where vehicle_id > ?)";
+const PREV_RENTAL_QUERY = "select * from rentals where vehicle_id = (select max(vehicle_id) from rentals where vehicle_id < ?)";
+
+
+const CAR_TYPE_QUERY = "select * from vehicles, vehicle_types where vehicles.license_plate_number = ? and vehicles.vehicle_type = vehicle_types.vin"
+
+
+const UNIQUE_RENTALS_QUERY = "select distinct vehicle_id from rentals";
+
 /** Queries //just didn't bother making them variables yet
  customer rentals history;
 select * from rentals, customers, vehicle_types, vehicles where rentals.customer_id = ? and rentals.vehicle_id = vehicles.license_plate_number and vehicles.vehicle_type = vehicle_types.vin; 
@@ -29,8 +43,15 @@ select * from rentals, vehicle_types, vehicles where rentals.vehicle_id = ? and 
 late return report;
 select * from rentals, customers where rentals.customer_id = customers.customer_id and rentals.return_condition is null;
 
+
 vehicle turnover report
-select * from rentals,vehicles, vehicle_types where rentals.vehicle_id = vehicles.license_plate_number and vehicles.vehicle_type = vehicle_types.vin;
+1) get unique vehicles
+    select distinct vehicle_id from rentals;
+2) send that back to the user with the turnover rate for the first vehicle
+3) user clicks next and searches for the remaining  
+select * from rentals,vehicles, vehicle_types where rentals.vehicle_id = ? and rentals.vehicle_id = vehicles.license_plate_number and vehicles.vehicle_type = vehicle_types.vin;
+
+
 
 vehicle availability report (how would i do this?)
 
@@ -42,7 +63,23 @@ select * from rentals,vehicles, vehicle_types where rentals.vehicle_id = ? and v
 default customer_rental query
  */
 
+/* Query Template
 
+        const res = await new Promise((resolve,reject)=>{
+            conn.query(UNIQUE_RENTALS_QUERY, (err,results)=>{
+                    if(err) {
+                        reject(new Error(err.message))
+                    }
+                    else if(!results.length) {
+                        reject(NO_RESULTS);
+                    }
+                    else{//do something with data
+
+                    }
+                })
+            });
+        return res;
+*/
 
 const NONE_ = "No Data!";
 
@@ -88,9 +125,72 @@ class RequestHandler{
 
     }
 
-    async turnover_report()
-    {
+    async vehicleType(vid){
+        const res = await new Promise((resolve,reject)=>{
+            conn.query(CAR_TYPE_QUERY,[vid], (err,results)=>{
+                    if(err) {
+                        reject(new Error(err.message))
+                    }
+                    else if(!results.length) {
+                        reject(NO_RESULTS);
+                    }
+                    else{//do something with data
+                        resolve(results)
+                    }
+                })
+            });
+        return res;
+    }
 
+    //takes a vehicle id and gets all rentals related to said vehicle
+    //returns an object with the data and the vehicle info
+    async rentals(vid)
+    {
+        const res = await new Promise((resolve,reject)=>{
+            conn.query(RENTALS_QUERY,[vid], (err,results)=>{
+                    if(err) {
+                        reject(new Error(err.message))
+                    }
+                    else if(!results.length) {
+                        reject(NO_RESULTS);
+                    }
+                    else{//do something with data
+                        this.vehicleType(vid).then(res=>{
+                            let data = {rentals: results, typeinfo: res}
+
+                            resolve(JSON.stringify(data))
+
+                        }).catch(err => reject(new Error(err.message)))
+                    }
+                })
+            });
+        return res;
+
+    }
+
+
+    async getRentalVehicles(){
+        const res = await new Promise((resolve,reject)=>{
+            conn.query(UNIQUE_RENTALS_QUERY, (err,results)=>{
+                let data = []
+                if(err) reject(new Error(err.message))
+                else if(!results.length){
+                    reject(NO_RESULTS);
+                }else{
+                    results.forEach(res => {
+                        data.push(res.vehicle_id)
+                    });
+                    
+                    data = {ids:data,first:null};
+                    this.rentals(data.ids[0]).then(res=>{
+                        data.first = JSON.parse(res);
+                        resolve(JSON.stringify(data));
+                    }).catch( err => reject(new Error(err.message)))
+
+                }
+            })
+        });
+        return res;
     }
 
 
--- backend/request.js
+++ backend/request.js
@@ -8,6 +8,19 @@ const query = new RequestHandler();
 const INDEX_FILE =  "../index.html"
 const PORT = 8080;
 
+
+
+query.getRentalVehicles().then(data =>{
+    let temp = JSON.parse(data)
+    console.log(temp.ids)
+    console.log(temp.first.rentals)
+    console.log(temp.first.typeinfo)
+    
+})
+.catch(err => console.log(err))
+
+
+
 const isEmpty = (obj)=>{
     for(var i in obj) return false;
     return true;
